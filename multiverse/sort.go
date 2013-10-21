71b141034d7edf3b2d85a85f1e8e69635fd01653
Robert Wertman <robert.wertman@gmail.com>
2013-10-21 01:36:05 -0400

    Moved sorting to own file.

---
+++ multiverse/sort.go
@@ -0,0 +1,49 @@
+package multiverse
+
+import (
+	"strings"
+
+	"github.com/arbovm/levenshtein"
+)
+
+type resultsList struct {
+	cards      []*Card
+	searchTerm string
+}
+
+func (r *resultsList) Len() int {
+	return len(r.cards)
+}
+
+func (r *resultsList) Swap(i, j int) {
+	r.cards[i], r.cards[j] = r.cards[j], r.cards[i]
+}
+
+func (r *resultsList) Less(i, j int) bool {
+	iDist := len(r.searchTerm) * len(r.searchTerm)
+	jDist := len(r.searchTerm) * len(r.searchTerm)
+
+	for _, word := range strings.Split(strings.ToLower(r.cards[i].Name), " ") {
+		maxI := len(r.searchTerm)
+		if maxI > len(word) {
+			maxI = len(word)
+		}
+		dist := levenshtein.Distance(r.searchTerm, word[0:maxI])
+		if dist < iDist {
+			iDist = dist
+		}
+	}
+
+	for _, word := range strings.Split(strings.ToLower(r.cards[j].Name), " ") {
+		maxJ := len(r.searchTerm)
+		if maxJ > len(word) {
+			maxJ = len(word)
+		}
+		dist := levenshtein.Distance(r.searchTerm, word[0:maxJ])
+		if dist < jDist {
+			jDist = dist
+		}
+	}
+
+	return iDist < jDist
+}

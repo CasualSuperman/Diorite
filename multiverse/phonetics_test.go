a53b5e29ee3724b43165bbe205582668eeca8e86
Robert Wertman <robert.wertman@gmail.com>
2013-10-27 07:00:50 -0400

    Started regression tests for searching Multiverse.
    Also added some benchmarks to help track our progress with real-time
    searching.

---
+++ multiverse/phonetics_test.go
@@ -0,0 +1,49 @@
+package multiverse
+
+import (
+	"os"
+	"testing"
+)
+
+func openTestingMultiverse() Multiverse {
+	file, _ := os.Open("/home/rwertman/.diorite/multiverse.mtg")
+	m, _ := Read(file)
+
+	m.Initialize()
+	return m
+}
+
+func TestPhoneticCorrection(t *testing.T) {
+	m := openTestingMultiverse()
+
+	if results := m.FuzzyNameSearch("asheok", 1); results[0].Name != "Ashiok, Nightmare Weaver" {
+		t.Errorf("Searching for 'asheok' should return Ashiok, Nightmare Weaver. Got: %s\n", results[0].Name)
+	}
+}
+
+func BenchmarkSingleWordFuzzyNameSearch(b *testing.B) {
+	m := openTestingMultiverse()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		m.FuzzyNameSearch("asheok", 15)
+	}
+}
+
+func BenchmarkMultiplePartialWordFuzzyNameSearch(b *testing.B) {
+	m := openTestingMultiverse()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		m.FuzzyNameSearch("ava me", 15)
+	}
+}
+
+func BenchmarkSingleLetterFuzzyNameSearch(b *testing.B) {
+	m := openTestingMultiverse()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		m.FuzzyNameSearch("d", 15)
+	}
+}

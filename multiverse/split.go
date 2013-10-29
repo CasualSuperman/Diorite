5ba42e485342f54bdd9f1ee99357c0a3656fa8b9
Robert Wertman <robert.wertman@gmail.com>
2013-10-29 03:42:58 -0400

    Wrote my own strings.Split
    We were spending almost 40% of our search time in it!

---
+++ multiverse/split.go
@@ -0,0 +1,29 @@
+package multiverse
+
+func Split(input string) []string {
+	start := 0
+	end := 0
+	l := len(input)
+	size := 1
+
+	for i := 0; i < l; i++ {
+		if input[i] == ' ' {
+			size++
+		}
+	}
+
+	s := make([]string, size)
+
+	for i := 0; i < size; i++ {
+		for end < l && input[end] != ' ' {
+			end++
+		}
+
+		s[i] = input[start:end]
+
+		end++
+		start = end
+	}
+
+	return s
+}
---
+++ multiverse/split_test.go
@@ -0,0 +1,14 @@
+package multiverse
+
+import "testing"
+
+func TestSplit(t *testing.T) {
+	s := Split("The quick brown fix jumps over the lazy dog.")
+	expected := []string{"The", "quick", "brown", "fix", "jumps", "over", "the", "lazy", "dog."}
+
+	for i, str := range s {
+		if expected[i] != str {
+			t.Error("Split failed.")
+		}
+	}
+}

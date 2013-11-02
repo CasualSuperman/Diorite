10d2377c1ffd02dbe73130d46b8e7b5be1fb2364
Robert Wertman <robert.wertman@gmail.com>
2013-11-02 03:12:22 -0400

    Wrote a Map function.

--- multiverse/search.go
+++ multiverse/search.go
@@ -4,13 +4,14 @@ import (
 	"runtime"
 )
 
-type Filter func(*Card) bool
+type FilterFunc func(*Card) bool
+type MapFunc func(*Card) interface{}
 
-func (c CardList) Filter(f Filter) CardList {
+func (c CardList) Filter(f FilterFunc) CardList {
 	cores := runtime.GOMAXPROCS(-1)
 	sectionLen := c.Len() / cores
 
-	cardChan := make(chan *Card, 16)
+	cardChan := make(chan *Card, cores*4)
 	doneChan := make(chan bool)
 	list := CardList(make([]scrubbedCard, 0))
 
@@ -37,6 +38,50 @@ func (c CardList) Filter(f Filter) CardList {
 			cores--
 		case c := <-cardChan:
 			list.Add(c)
+		}
+	}
+
+	return list
+}
+
+func (c CardList) Map(m MapFunc) []interface{} {
+	cores := runtime.GOMAXPROCS(-1)
+	sectionLen := c.Len() / cores
+
+	cardChan := make(chan interface{}, cores*4)
+	doneChan := make(chan bool)
+	var list []interface{}
+
+	for i := 0; i < cores; i++ {
+		start := sectionLen * i
+		end := start + sectionLen
+		if i == cores {
+			end = c.Len()
+		}
+
+		go func(start, end int) {
+			for _, card := range c[start:end] {
+				cardChan <- m(card.Card)
+			}
+			doneChan <- true
+		}(start, end)
+	}
+
+	for cores > 0 {
+		select {
+		case <-doneChan:
+			cores--
+		case c := <-cardChan:
+			l := len(list)
+			if len(list) < cap(list) {
+				list = list[:l]
+				list[l] = c
+			} else {
+				nList := make([]interface{}, l+1, l*2)
+				copy(nList, list)
+				nList[l] = c
+				list = nList
+			}
 		}
 	}
 

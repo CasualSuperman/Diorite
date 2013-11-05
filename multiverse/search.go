c6b761be319d9b0959e4f399d3cb70d59631bb6f
Robert Wertman <robert.wertman@gmail.com>
2013-11-05 03:35:28 -0500

    Redid search structure and started implementation.
    Design based off of the gosexy/db library.

--- multiverse/search.go
+++ multiverse/search.go
@@ -1,19 +1,23 @@
 package multiverse
 
 import (
+	"fmt"
 	"runtime"
 )
 
-type FilterFunc func(*Card) bool
-type MapFunc func(*Card) interface{}
+type Filter interface {
+	Ok(*Card) (bool, error)
+}
 
-func (c CardList) Filter(f FilterFunc) CardList {
+func (m Multiverse) Search(f Filter) ([]*Card, error) {
+	c := m.Cards.List
 	cores := runtime.GOMAXPROCS(-1)
 	sectionLen := c.Len() / cores
 
 	cardChan := make(chan *Card, cores*4)
 	doneChan := make(chan bool)
-	list := CardList(make([]scrubbedCard, 0))
+	errChan := make(chan error)
+	list := make([]*Card, 0, 1)
 
 	for i := 0; i < cores; i++ {
 		start := sectionLen * i
@@ -24,7 +28,12 @@ func (c CardList) Filter(f FilterFunc) CardList {
 
 		go func(start, end int) {
 			for _, card := range c[start:end] {
-				if f(card.Card) {
+				ok, err := f.Ok(card.Card)
+				if err != nil {
+					errChan <- err
+					return
+				}
+				if ok {
 					cardChan <- card.Card
 				}
 			}
@@ -37,53 +46,90 @@ func (c CardList) Filter(f FilterFunc) CardList {
 		case <-doneChan:
 			cores--
 		case c := <-cardChan:
-			list.Add(c)
+			for _, card := range list {
+				if card == c {
+					continue
+				}
+			}
+			l := len(list)
+			if l < cap(list) {
+				list = list[:l+1]
+			} else {
+				newList := make([]*Card, l+1, l*2)
+				copy(newList, list)
+				list = newList
+			}
+			list[l] = c
+		case err := <-errChan:
+			return nil, err
 		}
 	}
 
-	return list
+	shortList := make([]*Card, len(list))
+
+	for i, card := range list {
+		shortList[i] = card
+	}
+
+	return shortList, nil
 }
 
-func (c CardList) Map(m MapFunc) []interface{} {
-	cores := runtime.GOMAXPROCS(-1)
-	sectionLen := c.Len() / cores
+type Not struct {
+	f Filter
+}
 
-	cardChan := make(chan interface{}, cores*4)
-	doneChan := make(chan bool)
-	var list []interface{}
+func (n Not) Ok(c *Card) (bool, error) {
+	ok, err := n.f.Ok(c)
+	return !ok, err
+}
 
-	for i := 0; i < cores; i++ {
-		start := sectionLen * i
-		end := start + sectionLen
-		if i == cores {
-			end = c.Len()
-		}
+type And []Filter
 
-		go func(start, end int) {
-			for _, card := range c[start:end] {
-				cardChan <- m(card.Card)
-			}
-			doneChan <- true
-		}(start, end)
-	}
-
-	for cores > 0 {
-		select {
-		case <-doneChan:
-			cores--
-		case c := <-cardChan:
-			l := len(list)
-			if len(list) < cap(list) {
-				list = list[:l]
-				list[l] = c
-			} else {
-				nList := make([]interface{}, l+1, l*2)
-				copy(nList, list)
-				nList[l] = c
-				list = nList
-			}
+func (a And) Ok(c *Card) (bool, error) {
+	for _, f := range a {
+		ok, err := f.Ok(c)
+		if !ok || err != nil {
+			return ok, err
 		}
 	}
+	return true, nil
+}
 
-	return list
+type Or []Filter
+
+func (o Or) Ok(c *Card) (bool, error) {
+	for _, f := range o {
+		ok, err := f.Ok(c)
+		if ok || err != nil {
+			return ok, err
+		}
+	}
+	return false, nil
+}
+
+type Cond map[string]interface{}
+
+func (c Cond) Ok(card *Card) (bool, error) {
+	for key, val := range c {
+		switch key {
+		case "color", "colors":
+			same, err := handleColorSearch(card.Colors, val)
+			if err != nil || !same {
+				return same, err
+			}
+		}
+	}
+	return true, nil
+}
+
+func handleColorSearch(cardColor ManaColor, val interface{}) (bool, error) {
+	switch val := val.(type) {
+	case ManaColor:
+		if cardColor&val != 0 {
+			return true, nil
+		}
+		return false, nil
+	}
+
+	return false, fmt.Errorf("unexpected color type %T", val)
 }

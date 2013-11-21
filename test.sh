f16d9a473cb9f528e9c8dad3984eb43a7d5b84d8
Robert Wertman <robert.wertman@gmail.com>
2013-11-21 03:00:50 -0500

    Updated test script to fail on any error.
    Also added comments.

--- test.sh
+++ test.sh
@@ -1,7 +1,20 @@
+set -e
+
+# Run our tests
 go test ./multiverse
+
+# Compile the binaries
 go build -o server.bin ./server
 go build -o diorite.bin
 
+# Start the server
 ./server.bin -travis &
-sleep 1
+
+# Wait for it to bind
+sleep 0.1
+
+# Start our client
 ./diorite.bin -local
+
+# Clean up
+rm diorite.bin server.bin

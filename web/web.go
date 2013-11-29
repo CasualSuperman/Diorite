a6347c67a729ffc34d583a70b00f2566d3f11cff
Robert Wertman <robert.wertman@gmail.com>
2013-11-29 03:00:32 -0500

    Added search-as-you-type.

--- web/web.go
+++ web/web.go
@@ -3,6 +3,7 @@ package web
 import (
 	"net/http"
 
+	ws "code.google.com/p/go.net/websocket"
 	m "github.com/CasualSuperman/Diorite/multiverse"
 )
 
@@ -12,6 +13,7 @@ func Serve(mult m.Multiverse) {
 	multiverse = mult
 	http.Handle("/", http.FileServer(http.Dir("./web/static")))
 	http.Handle("/name/", http.StripPrefix("/name/", http.HandlerFunc(nameSearch)))
+	http.Handle("/ws", ws.Handler(websocketServer))
 	http.ListenAndServe(":6060", nil)
 }
 
@@ -22,5 +24,45 @@ func nameSearch(rq http.ResponseWriter, req *http.Request) {
 		rq.Write([]byte("Unable to locate card."))
 	} else {
 		rq.Write([]byte(cards[0].String()))
+	}
+}
+
+type wsRequest struct {
+	Type string
+	Request string
+}
+
+type wsResponse struct {
+	Type string
+	Response interface{}
+}
+
+func websocketServer(w *ws.Conn) {
+	open := true
+
+	for open {
+		var msg wsRequest
+		err := ws.JSON.Receive(w, &msg)
+		if err != nil {
+			println(err.Error())
+			break
+		}
+
+		switch msg.Type {
+		case "close":
+			open = false
+		case "nameSearch":
+			cards := multiverse.FuzzyNameSearch(msg.Request, 15)
+			names := make([]string, len(cards))
+			for i, card := range cards {
+				names[i] = card.Name
+			}
+			ws.JSON.Send(w, wsResponse{
+				"nameSearch",
+				names,
+			})
+		default:
+			println("Type:", msg.Type)
+		}
 	}
 }

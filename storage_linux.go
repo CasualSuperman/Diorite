0069954ff36d0f970c607b856041ba852b463e50
Robert Wertman <robert.wertman@gmail.com>
2013-12-27 03:24:26 -0500

    $XDG_DATA_HOME is cool.

--- storage_linux.go
+++ storage_linux.go
@@ -2,4 +2,11 @@ package main
 
 import "os"
 
-var defaultLocation = os.ExpandEnv("${HOME}/.local/share/diorite/multiverse.mtg")
+var defaultLocation string
+func init() {
+	if os.ExpandEnv("${XDG_DATA_HOME}") != "" {
+		defaultLocation = os.ExpandEnv("${XDG_DATA_HOME}/diorite/multiverse.mtg")
+	} else {
+		defaultLocation = os.ExpandEnv("${HOME}/.local/share/diorite/multiverse.mtg")
+	}
+}
